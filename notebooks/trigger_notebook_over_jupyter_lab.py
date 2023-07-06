f520ad075c34e83c0631dcc78ce21f5d96a689ae
Földi Tamás <tfoldi@xsi.hu>
2023-07-06 09:49:12 +0200

    chore: add command line arguments for passing ctx

--- notebooks/trigger_notebook_over_jupyter_lab.py
+++ notebooks/trigger_notebook_over_jupyter_lab.py
@@ -2,33 +2,63 @@
 #
 # Based on https://stackoverflow.com/a/54551221/942520
 
-
+import os
+import sys
+import argparse
 import json
 import requests
 import datetime
 import uuid
-import os
+import argparse
 from pprint import pprint
 from websocket import create_connection, WebSocketTimeoutException
 
+
+# Create an argument parser
+parser = argparse.ArgumentParser()
+
+# Add positional arguments
+parser.add_argument(
+    "jupyter_server",
+    nargs="?",
+    default=os.getenv("JUPYTER_SERVER"),
+    help="Jupyter server address and port",
+)
+parser.add_argument(
+    "jupyter_notebook",
+    nargs="?",
+    default=os.getenv("JUPYTER_NOTEBOOK"),
+    help="Notebook path on Jupyter server with a leading slash",
+)
+parser.add_argument(
+    "--use-https", action="store_true", default=False, help="Use https instead of http"
+)
+parser.add_argument(
+    "--verbose", action="store_true", default=False, help="Print debug messages"
+)
+
+# Parse the command line arguments
+args = parser.parse_args()
+
 # Notebook path on Jupyter server with a leading slash
-notebook_path = "/Vizallas.ipynb"
+notebook_path = args.jupyter_notebook
 # Either http or https
-protocol = "http"
+protocol = "https" if args.use_https else "http"
 # Jupyter server address and port
-jupyter_server = "localhost:8888"
+jupyter_server = args.jupyter_server
 headers = {"Authorization": f"Token {os.environ['JUPYTER_TOKEN']}"}
 
 # Create a kernel
 url = f"{protocol}://{jupyter_server}/api/kernels"
-print("Creating a new kernel")
+print(f"Creating a new kernel at {url}", file=sys.stderr)
 with requests.post(url, headers=headers) as response:
     kernel = json.loads(response.text)
 
 # Load the notebook and get the code of each cell
 url = f"{protocol}://{jupyter_server}/api/contents{notebook_path}"
-with requests.post(url, headers=headers) as response:
+with requests.get(url, headers=headers) as response:
     file = json.loads(response.text)
+pprint({"notebook_content": file}, sys.stderr) if args.verbose else None
 # filter out non-code cells like markdown
 code = [
     c["source"]
@@ -58,9 +88,9 @@ def send_execute_request(code):
     return msg
 
 
-print("Sending execution requests for each cell")
+print("Sending execution requests for each cell", file=sys.stderr)
 for c in code:
-    ws.send(json.dump(send_execute_request(c)))
+    ws.send(json.dumps(send_execute_request(c)))
 
 code_blocks_to_execute = len(code)
 
@@ -69,13 +99,14 @@ while code_blocks_to_execute > 0:
         rsp = json.loads(ws.recv())
         msg_type = rsp["msg_type"]
         if msg_type == "error":
+            pprint({"exception": rsp["content"]}, sys.stderr)
             raise Exception(rsp["content"]["traceback"][0])
-    except WebSocketTimeoutException as _e:
-        pprint(_e)
+    except Exception as _e:
+        print(_e, sys.stderr)
         break
-    print(
-        f"Received message of type {msg_type} with text {rsp['content'].get('text','')}"
-    )
+
+    if msg_type == "execute_result" or args.verbose:
+        pprint(rsp["content"])
 
     if (
         msg_type == "execute_reply"
@@ -85,10 +116,10 @@ while code_blocks_to_execute > 0:
         code_blocks_to_execute -= 1
 
 
-print("Processing finished. Closing websocket connection")
+print("Processing finished. Closing websocket connection", file=sys.stderr)
 ws.close()
 
 # Delete the kernel
-print("Deleting kernel")
+print("Deleting kernel", file=sys.stderr)
 url = f"{protocol}://{jupyter_server}/api/kernels/{kernel['id']}"
 response = requests.delete(url, headers=headers)
