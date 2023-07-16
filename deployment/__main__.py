05dbca58037bd66218d229dbe31aae7731354f28
Földi Tamás <tfoldi@xsi.hu>
2023-07-16 10:06:33 +0200

    feat: deploy to cloudrun without cloudsql

--- deployment/__main__.py
+++ deployment/__main__.py
@@ -8,53 +8,71 @@ from pulumi_gcp.cloudrun import (
 
 config = Config()
 
-cloud_sql_instance = pulumi_gcp.sql.DatabaseInstance(
-    "pgsql-instance",
-    database_version="POSTGRES_12",
-    deletion_protection=False, # TODO: True if production
-    settings=pulumi_gcp.sql.DatabaseInstanceSettingsArgs(tier="db-f1-micro"), # TODO: from config
-)
+if config.get_bool("use_cloud_sql"):
+    cloud_sql_instance = pulumi_gcp.sql.DatabaseInstance(
+        "pgsql-instance",
+        database_version="POSTGRES_12",
+        deletion_protection=False,  # TODO: True if production
+        settings=pulumi_gcp.sql.DatabaseInstanceSettingsArgs(
+            tier="db-f1-micro"
+        ),  # TODO: from config
+    )
 
-database = pulumi_gcp.sql.Database(
-    "database", instance=cloud_sql_instance.name, name=config.require("db-name")
-)
+    database = pulumi_gcp.sql.Database(
+        "database", instance=cloud_sql_instance.name, name=config.require("db-name")
+    )
 
-users = pulumi_gcp.sql.User(
-    "users",
-    name=config.require("db-name"),
-    instance=cloud_sql_instance.name,
-    password=config.require_secret("db-password"), # TODO: Pulumi.random if not specified
-)
+    users = pulumi_gcp.sql.User(
+        "users",
+        name=config.require("db-name"),
+        instance=cloud_sql_instance.name,
+        password=config.require_secret(
+            "db-password"
+        ),  # TODO: Pulumi.random if not specified
+    )
 
-sql_instance_url = Output.concat(
-    "postgres://",
-    config.require("db-name"),
-    ":",
-    config.require_secret("db-password"),
-    "@/",
-    config.require("db-name"),
-    "?host=/cloudsql/",
-    cloud_sql_instance.connection_name,
-)
+    sql_instance_url = Output.concat(
+        "postgres://",
+        config.require("db-name"),
+        ":",
+        config.require_secret("db-password"),
+        "@/",
+        config.require("db-name"),
+        "?host=/cloudsql/",
+        cloud_sql_instance.connection_name,
+    )
+
+    service_annotations = {
+        "run.googleapis.com/cloudsql-instances": cloud_sql_instance.connection_name
+    }
+else:
+    service_annotations = {}
+    sql_instance_url = config.require("db-url")
 
 cloud_run = pulumi_gcp.cloudrun.Service(
-    "default-service",
+    "vizallas-notebook-service",
     location=Config("gcp").require("region"),
     template=pulumi_gcp.cloudrun.ServiceTemplateArgs(
         metadata=ServiceTemplateMetadataArgs(
-            annotations={
-                "run.googleapis.com/cloudsql-instances": cloud_sql_instance.connection_name
-            }
+            annotations=service_annotations,
         ),
         spec=pulumi_gcp.cloudrun.ServiceTemplateSpecArgs(
             containers=[
                 pulumi_gcp.cloudrun.ServiceTemplateSpecContainerArgs(
-                    image="gcr.io/cloudrun/hello",
+                    image="tfoldi/vizallas-notebooks",
                     envs=[
                         ServiceTemplateSpecContainerEnvArgs(
                             name="PG_URL",
                             value=sql_instance_url,
-                        )
+                        ),
+                        ServiceTemplateSpecContainerEnvArgs(
+                            name="JUPTER_TOKEN", value=config.get("jupyter_token")
+                        ),
+                    ],
+                    ports=[
+                        pulumi_gcp.cloudrun.ServiceTemplateSpecContainerPortArgs(
+                            container_port=8888,
+                        ),
                     ],
                 )
             ],
@@ -68,5 +86,17 @@ cloud_run = pulumi_gcp.cloudrun.Service(
     ],
 )
 
-pulumi.export("pgsql_instance_name", cloud_sql_instance.name)
+# Create an IAM member to make the service publicly accessible.
+invoker = pulumi_gcp.cloudrun.IamMember(
+    "invoker",
+    pulumi_gcp.cloudrun.IamMemberArgs(
+        location=cloud_run.location,
+        service=cloud_run.name,
+        role="roles/run.invoker",
+        member="allUsers",
+    ),
+)
+
+if config.get_bool("use_cloud_sql"):
+    pulumi.export("pgsql_instance_name", cloud_sql_instance.name)
 pulumi.export("cloud_run_url", cloud_run.statuses[0].url)
--- notebooks/trigger_notebook_over_jupyter_lab.py
+++ notebooks/trigger_notebook_over_jupyter_lab.py
@@ -13,64 +13,51 @@ import argparse
 from pprint import pprint
 from websocket import create_connection, WebSocketTimeoutException
 
+if "JUPYTER_TOKEN" not in os.environ:
+    raise Exception("JUPYTER_TOKEN environment variable must be set")
 
-# Create an argument parser
-parser = argparse.ArgumentParser()
 
-# Add positional arguments
-parser.add_argument(
-    "jupyter_server",
-    nargs="?",
-    default=os.getenv("JUPYTER_SERVER"),
-    help="Jupyter server address and port",
-)
-parser.add_argument(
-    "jupyter_notebook",
-    nargs="?",
-    default=os.getenv("JUPYTER_NOTEBOOK"),
-    help="Notebook path on Jupyter server with a leading slash",
-)
-parser.add_argument(
-    "--use-https", action="store_true", default=False, help="Use https instead of http"
-)
-parser.add_argument(
-    "--verbose", action="store_true", default=False, help="Print debug messages"
-)
+def parse_args(event):
+    # Create an argument parser
+    parser = argparse.ArgumentParser()
 
-# Parse the command line arguments
-args = parser.parse_args()
+    # Add positional arguments
+    parser.add_argument(
+        "jupyter_server",
+        nargs="?",
+        default=os.getenv("JUPYTER_SERVER"),
+        help="Jupyter server address and port",
+    )
+    parser.add_argument(
+        "jupyter_notebook",
+        nargs="?",
+        default=os.getenv("JUPYTER_NOTEBOOK"),
+        help="Notebook path on Jupyter server with a leading slash",
+    )
+    parser.add_argument(
+        "--use-https",
+        action="store_true",
+        default=False,
+        help="Use https instead of http",
+    )
+    parser.add_argument(
+        "--verbose", action="store_true", default=False, help="Print debug messages"
+    )
 
-# Notebook path on Jupyter server with a leading slash
-notebook_path = args.jupyter_notebook
-# Either http or https
-protocol = "https" if args.use_https else "http"
-# Jupyter server address and port
-jupyter_server = args.jupyter_server
-headers = {"Authorization": f"Token {os.environ['JUPYTER_TOKEN']}"}
+    # Parse the command line arguments or use the event payload
+    if event is not None and "data" in event:
+        args = parser.parse_args(
+            event["data"].split()
+        )  # Split the payload into a list of arguments
+    else:
+        args = parser.parse_args()
 
-# Create a kernel
-url = f"{protocol}://{jupyter_server}/api/kernels"
-print(f"Creating a new kernel at {url}", file=sys.stderr)
-with requests.post(url, headers=headers) as response:
-    kernel = json.loads(response.text)
+    # Print help if no arguments are provided
+    if args.jupyter_server is None or args.jupyter_notebook is None:
+        parser.print_help()
+        sys.exit(1)
 
-# Load the notebook and get the code of each cell
-url = f"{protocol}://{jupyter_server}/api/contents{notebook_path}"
-with requests.get(url, headers=headers) as response:
-    file = json.loads(response.text)
-pprint({"notebook_content": file}, sys.stderr) if args.verbose else None
-# filter out non-code cells like markdown
-code = [
-    c["source"]
-    for c in file["content"]["cells"]
-    if len(c["source"]) > 0 and c["cell_type"] == "code"
-]
-
-# Execution request/reply is done on websockets channels
-ws = create_connection(
-    f"{'ws' if protocol == 'http' else 'wss'}://{jupyter_server}/api/kernels/{kernel['id']}/channels",
-    header=headers,
-)
+    return args
 
 
 def send_execute_request(code):
@@ -88,38 +75,80 @@ def send_execute_request(code):
     return msg
 
 
-print("Sending execution requests for each cell", file=sys.stderr)
-for c in code:
-    ws.send(json.dumps(send_execute_request(c)))
+# Main function
+# This is the entrypoint for both local and cloud execution
+# The event and context arguments are only used when running in the cloud
+def main(event, context):
+    args = parse_args(event)
 
-code_blocks_to_execute = len(code)
+    # Notebook path on Jupyter server with a leading slash
+    notebook_path = args.jupyter_notebook
+    # Either http or https
+    protocol = "https" if args.use_https else "http"
+    # Jupyter server address and port
+    jupyter_server = args.jupyter_server
+    headers = {"Authorization": f"Token {os.environ['JUPYTER_TOKEN']}"}
 
-while code_blocks_to_execute > 0:
-    try:
-        rsp = json.loads(ws.recv())
-        msg_type = rsp["msg_type"]
-        if msg_type == "error":
-            pprint({"exception": rsp["content"]}, sys.stderr)
-            raise Exception(rsp["content"]["traceback"][0])
-    except Exception as _e:
-        print(_e, sys.stderr)
-        break
+    # Create a kernel
+    url = f"{protocol}://{jupyter_server}/api/kernels"
+    print(f"Creating a new kernel at {url}", file=sys.stderr)
+    with requests.post(url, headers=headers) as response:
+        pprint({"kernel": response.headers}, sys.stderr) if args.verbose else None
+        kernel = json.loads(response.text)
 
-    if msg_type == "execute_result" or args.verbose:
-        pprint(rsp["content"])
+    # Load the notebook and get the code of each cell
+    url = f"{protocol}://{jupyter_server}/api/contents{notebook_path}"
+    with requests.get(url, headers=headers) as response:
+        file = json.loads(response.text)
+    pprint({"notebook_content": file}, sys.stderr) if args.verbose else None
+    # filter out non-code cells like markdown
+    code = [
+        c["source"]
+        for c in file["content"]["cells"]
+        if len(c["source"]) > 0 and c["cell_type"] == "code"
+    ]
 
-    if (
-        msg_type == "execute_reply"
-        and rsp["metadata"].get("status") == "ok"
-        and rsp["metadata"].get("dependencies_met", False)
-    ):
-        code_blocks_to_execute -= 1
+    # Execution request/reply is done on websockets channels
+    ws = create_connection(
+        f"{'ws' if protocol == 'http' else 'wss'}://{jupyter_server}/api/kernels/{kernel['id']}/channels",
+        header=headers,
+    )
+
+    print("Sending execution requests for each cell", file=sys.stderr)
+    for c in code:
+        ws.send(json.dumps(send_execute_request(c)))
+
+    code_blocks_to_execute = len(code)
+
+    while code_blocks_to_execute > 0:
+        try:
+            rsp = json.loads(ws.recv())
+            msg_type = rsp["msg_type"]
+            if msg_type == "error":
+                pprint({"exception": rsp["content"]}, sys.stderr)
+                raise Exception(rsp["content"]["traceback"][0])
+        except Exception as _e:
+            print(_e, sys.stderr)
+            break
+
+        if msg_type == "execute_result" or args.verbose:
+            pprint(rsp["content"])
+
+        if (
+            msg_type == "execute_reply"
+            and rsp["metadata"].get("status") == "ok"
+            and rsp["metadata"].get("dependencies_met", False)
+        ):
+            code_blocks_to_execute -= 1
+
+    print("Processing finished. Closing websocket connection", file=sys.stderr)
+    ws.close()
+
+    # Delete the kernel
+    print("Deleting kernel", file=sys.stderr)
+    url = f"{protocol}://{jupyter_server}/api/kernels/{kernel['id']}"
+    response = requests.delete(url, headers=headers)
 
 
-print("Processing finished. Closing websocket connection", file=sys.stderr)
-ws.close()
-
-# Delete the kernel
-print("Deleting kernel", file=sys.stderr)
-url = f"{protocol}://{jupyter_server}/api/kernels/{kernel['id']}"
-response = requests.delete(url, headers=headers)
+if __name__ == "__main__":
+    main(None, None)
