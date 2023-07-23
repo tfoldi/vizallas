9743e359044adbd8f7bb1ae1730a8d8dd1d83113
Földi Tamás <tfoldi@xsi.hu>
2023-07-23 10:08:41 +0200

    chore: add Secrets for XCode Cloud

---
+++ frontend/vizallas/vizallas/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  _Secrets.swift
+//  vizallas
+//
+//  Created by Tamas Foldi on 2023. 07. 18..
+//
+
+import Foundation
+
+enum Secrets {
+    static let supabaseURL = URL(string: "https://pqggdnyrsqkbngzfhnhj.supabase.co")!
+    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZ2dkbnlyc3FrYm5nemZobmhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0OTA5NTgsImV4cCI6MjAwNTA2Njk1OH0.B3hoFN5fPFISb-OANIvgHFpjJnpIRiKSTSuI7YE5wCA"
+}
